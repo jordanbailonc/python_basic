25d5ba573133de8090d11449c629f670bb3587fa
Jordan Bailon Cercado <jordanfbc@hotmail.com>
2022-09-21 14:07:17 +0200

    Python Program to Check Armstrong Number

---
+++ Exercise 11-20/ex_19.py
@@ -0,0 +1,26 @@
+"""Python Program to Check Armstrong Number"""
+
+class Armstrong:
+    def __init__(self,input_num) -> None:
+        self.input_num=input_num
+
+    def check_armstrong(self):
+        sum=0
+        index =(len(str(self.input_num)))
+        aux=self.input_num
+        while aux >0:
+            my_digit = aux % 10
+            sum += my_digit ** index
+            aux //=10
+
+        if self.input_num==sum:
+            print('{0} is an Armstrong number'.format(self.input_num))
+        else:
+            print('{0} is not an Armstong number.'.format(self.input_num))
+
+try:
+    number_check=int(input('Write the integer number to check:\t'))
+    my_Armstrong=Armstrong(number_check)
+    my_Armstrong.check_armstrong()
+except Exception as ex:
+    print(ex)
