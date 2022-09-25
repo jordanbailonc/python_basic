f4122e474acd110b4bf5e6a5888bbe00af57b178
Jordan Bailon <jordanbailonc@gmail.com>
2022-09-25 23:49:15 +0200

    Python Program to Display Powers of 2 Using Anonymous Function

---
+++ Exercise 21-30/ex_22.py
@@ -0,0 +1,12 @@
+"""Python Program to Display Powers of 2 Using Anonymous Function"""
+#python lambda function example 1
+def to_sume():
+    sume = lambda x,y: x+y 
+    print(sume(5,4))
+#example 2
+def to_rest():
+    rest = lambda x,y: x-y
+    print(rest(5,1))
+
+to_sume()
+to_rest()
