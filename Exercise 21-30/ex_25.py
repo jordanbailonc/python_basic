68a19df62f6dc89c1c59bb73efc5d7c3d054ddd3
Jordan Bailon <jordanbailonc@gmail.com>
2022-09-27 16:16:56 +0200

    Python Program to Find ASCII Value of Character

--- Exercise 21-30/ex_25.py
+++ Exercise 21-30/ex_25.py
@@ -1,29 +1,17 @@
-"""Python Program to Find HCF or GCD"""
+"""Python Program to Find ASCII Value of Character"""
+class val_char:
+    def __init__(self,char) -> None:
+        self.char=char
 
-#class to get two numbers
-class getGCD:
-    def __init__(self,number,another) -> None:
-        self.number=number
-        self.another=another
-
-    def calculate(self):
-        gcd=0
-        if self.number>self.another:
-            max=self.number
-        else:
-            max= self.another
-        for i in range (2,max):
-            if(self.number % i ==0)and (self.another % i ==0):
-                gcd = i
-        if(gcd==0):
-            print("There's no GCD for {0} and {1}".format(self.number,self.another))
-        else:
-            print('The GCD of the {0} and {1} is {2}'.format(self.number,self.another,gcd))
+    def print_ord(self):
+        print(ord(self.char))
 
 try:
-    first_data= int(input('Write the first number:\t'))
-    second_data = int(input('Write the second number:\t'))
-    myGCD= getGCD(first_data,second_data)
-    myGCD.calculate()
+    my_char=input('Write the characther you want ti converter: \t')
+    if(len(my_char)>1):
+        print('The input only admits one characther.')
+    else:
+        converter= val_char(my_char)
+        converter.print_ord()
 except:
-    print('Data input must be integer.')
+    print('Some error occurs.')
