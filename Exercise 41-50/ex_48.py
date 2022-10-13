cac441f83a2cb60fae4e929a4f953645a275a7a4
Jordan Bailon Cercado <jordanfbc@hotmail.com>
2022-10-13 16:21:39 +0200

    Program to Merge 2 Dictionaries

---
+++ Exercise 41-50/ex_48.py
@@ -0,0 +1,7 @@
+"""Python Program to Merge to Dictionaries"""
+
+my_Dictionary={1:'a' , 2:'b'}
+your_Dictionary ={1:'c', 4:'d'}
+
+print(my_Dictionary | your_Dictionary)
+##print({**my_Dictionary, **your_Dictionary})
