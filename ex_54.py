4fb1f816ee1306b99e4b2333dd3b481b49200e05
Jordan Bailon Cercado <jordanfbc@hotmail.com>
2022-11-11 14:35:45 +0100

    Python Program to order values of a Dictionary

---
+++ ex_54.py
@@ -0,0 +1,6 @@
+"""Python Program to Sort a Dictionary by Value"""
+
+dictionary ={"person1":"Jordan", "person2:":"Astrid","person3":"Lisbeth"}
+
+result = sorted(dictionary.values())
+print (result)
